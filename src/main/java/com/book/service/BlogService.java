452467ce2adb6186b7944335392944071bf449de
octo888 <vvmoroz88@gmail.com>
2015-07-01 21:30:52 +0300

    blog-item start

--- src/main/java/com/book/service/BlogService.java
+++ src/main/java/com/book/service/BlogService.java
@@ -1,8 +1,12 @@
 package com.book.service;
 
 import com.book.entity.Blog;
+import com.book.entity.User;
 import com.book.repository.BlogRepository;
+import com.book.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.method.P;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -14,15 +18,23 @@ public class BlogService {
     @Autowired
     private BlogRepository blogRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     public void save(Blog blog, String name) {
 
+        User user = userRepository.findByName(name);
+        blog.setUser(user);
+        blogRepository.save(blog);
+       // saveItems(blog);
     }
 
-    public void delete(Blog blog) {
-
+    @PreAuthorize("#blog.user.name == authentication.name or hasRole('ROLE_ADMIN')")
+    public void delete(@P("blog") Blog blog) {
+        blogRepository.delete(blog);
     }
 
     public Blog findOne(int id) {
-        return null;
+        return blogRepository.findOne(id);
     }
 }
